f4fb660872e665f5976145e7e85d15a2758c6f96
Baker, Hans - Student Rijn IJssel <00270562@rijnijssel.nl>
2022-10-19 11:17:29 +0200

    Finale

---
+++ yaeger-tutorial-master - kopie/yaeger-tutorial-master/src/main/java/com/github/hanyaeger/FirstBoss.java
@@ -0,0 +1,83 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package com.github.hanyaeger;
+
+import com.github.hanyaeger.FirstBoss.1;
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.Collided;
+import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.Direction;
+import com.github.hanyaeger.api.entities.Newtonian;
+import com.github.hanyaeger.api.entities.SceneBorderTouchingWatcher;
+import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+import com.github.hanyaeger.api.entities.impl.TextEntity;
+import com.github.hanyaeger.api.scenes.SceneBorder;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class FirstBoss extends DynamicSpriteEntity implements SceneBorderTouchingWatcher, Newtonian, Collided, Collider {
+    private int health = 20;
+    private int bossSpeed = 2;
+    private int counter = 0;
+    private TextEntity bossHealthText = new TextEntity(new Coordinate2D(0.0, 550.0));
+    private BossLaserSpawner bossLaser;
+    private Main main;
+
+    public FirstBoss(Coordinate2D location, Main main, BossLaserSpawner bossLaser) {
+        super("sprites/boss.png", location, new Size(65.0, 65.0), 1, 4);
+        this.main = main;
+        this.bossLaser = bossLaser;
+        this.setMotion((double)this.bossSpeed, 270.0);
+        this.bossHealthText.setText("Boss HP: " + this.health);
+        this.bossHealthText.setFont(Font.font("Roboto", FontWeight.NORMAL, 30.0));
+        this.bossHealthText.setFill(Color.DARKBLUE);
+        this.setGravityConstant(0.005);
+        this.setFrictionConstant(0.04);
+    }
+
+    public void notifyBoundaryTouching(SceneBorder border) {
+        switch (1.$SwitchMap$com$github$hanyaeger$api$scenes$SceneBorder[border.ordinal()]) {
+            case 1:
+                this.setMotion((double)this.bossSpeed, Direction.RIGHT);
+                break;
+            case 2:
+                this.setMotion((double)this.bossSpeed, Direction.LEFT);
+        }
+
+    }
+
+    public void onCollision(Collider collidingObject) {
+        if (collidingObject.getClass().equals(LaserBeam.class)) {
+            this.bossDamage();
+        }
+
+    }
+
+    public void bossDamage() {
+        if (this.health >= 0) {
+            --this.health;
+            ++this.counter;
+            this.bossHealthText.setText("Boss HP: " + this.health);
+            if (this.counter == 8) {
+                ++this.bossSpeed;
+                this.bossLaser.setFiringRate(200);
+                this.counter = 0;
+            }
+        } else {
+            System.out.println("YOU WON!");
+            this.remove();
+            this.main.setActiveScene(2);
+        }
+
+    }
+
+    public void Shoot() {
+        System.out.println("Boss Is Shooting");
+        this.bossLaser.boss = this;
+    }
+}
--- yaeger-tutorial-master - kopie/yaeger-tutorial-master/src/main/java/com/github/hanyaeger/LaserBeam.java
+++ yaeger-tutorial-master - kopie/yaeger-tutorial-master/src/main/java/com/github/hanyaeger/LaserBeam.java
@@ -1,32 +1,43 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.github.hanyaeger;
 
-import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.Collided;
 import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.Direction;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 
 public class LaserBeam extends DynamicSpriteEntity implements Collider, Collided {
-
     private Player player;
+    private FirstBoss boss;
 
-    public LaserBeam(Player player){
-        super("sprites/Beam.png", new Coordinate2D(player.getAnchorLocation().getX() + 20, player.getAnchorLocation().getY()));
+    public LaserBeam(Player player) {
+        super("sprites/Beam.png", player.getAnchorLocation());
         this.player = player;
-        setMotion(5, 180d);
+        System.out.println("Pew");
+        this.setMotion(5.0, 180.0);
     }
 
-    @Override
+    public LaserBeam(FirstBoss boss) {
+        super("sprites/Beam.png", boss.getAnchorLocation());
+        this.boss = boss;
+        System.out.println("BOOM");
+        this.setMotion(5.0, Direction.DOWN);
+    }
+
     public void onCollision(Collider collider) {
-        if (collider.getClass().equals(Asteroid1.class)){
-            //ADD POINT: 1
-            player.GainScore(((Asteroid1) collider).getPoint());
-            ((Asteroid1) collider).death();
-            remove();
-        }else if(collider.getClass().equals(Asteroid2.class)){
-            //ADD POINT: 2
-            player.GainScore(((Asteroid2) collider).getPoint());
-            ((Asteroid2) collider).death();
-            remove();
+        if (collider.getClass().equals(Asteroid1.class)) {
+            this.player.GainScore(((Asteroid1)collider).getPoint());
+            ((Asteroid1)collider).remove();
+            this.remove();
+        } else if (collider.getClass().equals(Asteroid2.class)) {
+            this.player.GainScore(((Asteroid2)collider).getPoint());
+            ((Asteroid2)collider).remove();
+            this.remove();
         }
+
     }
 }
